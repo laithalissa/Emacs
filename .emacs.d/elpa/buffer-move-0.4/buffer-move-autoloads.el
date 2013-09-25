57d415aab40d3239943a9290d8f58c4c4ee52590
Laith Alissa <laithalissa@gmail.com>
2013-09-25 21:22:33 +0100

    Added more missed libraries

---
+++ .emacs.d/elpa/buffer-move-0.4/buffer-move-autoloads.el
@@ -0,0 +1,16 @@
+;;; buffer-move-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("buffer-move.el") (20986 34620 548058
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; buffer-move-autoloads.el ends here
---
+++ .emacs.d/elpa/window-number-1.0.1/window-number-autoloads.el
@@ -0,0 +1,16 @@
+;;; window-number-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("window-number-pkg.el" "window-number.el")
+;;;;;;  (21035 39052 793526 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; window-number-autoloads.el ends here
